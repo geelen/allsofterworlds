e268a8edffc1892254bb993698173797f53762e8
Glen Maddern <glenmaddern@gmail.com>
2010-11-02 16:37:27 +1100

    fixes for firefox

--- app/public/scripts/allsofterworlds.js
+++ app/public/scripts/allsofterworlds.js
@@ -1,14 +1,16 @@
 (function(){
   var loadMoreContent, proportionScrolled;
   proportionScrolled = function() {
-    return ($('body').scrollTop() + $(window).height()) / $('body').height();
+    var prop;
+    prop = ($(window).scrollTop() + $(window).height()) / $('body').height();
+    console.log(prop);
+    return prop;
   };
   loadMoreContent = function() {
     var from;
     if (!$('#content').hasClass('loading')) {
       $('#content').addClass('loading');
       from = $('section.article:last p.heading a.original').html().replace(/\D/g, '');
-      console.log(from);
       return $.get(("/articles?from=" + (parseInt(from) - 1)), {}, function(data, textStatus) {
         textStatus === 'success' ? $('#content .spinner').before(data) : console.log("failed to load more content");
         return $('#content').removeClass('loading');
--- app/scripts/allsofterworlds.coffee
+++ app/scripts/allsofterworlds.coffee
@@ -1,9 +1,12 @@
-proportionScrolled = -> ($('body').scrollTop() + $(window).height()) / $('body').height()
+proportionScrolled = ->
+  prop = ($(window).scrollTop() + $(window).height()) / $('body').height()
+  console.log(prop)
+  prop
+
 loadMoreContent = ->
   if !$('#content').hasClass('loading')
     $('#content').addClass('loading')
     from = $('section.article:last p.heading a.original').html().replace(/\D/g, '')
-    console.log(from)
     $.get "/articles?from=${parseInt(from) - 1}", {}, (data, textStatus) ->
       if textStatus == 'success'
         $('#content .spinner').before(data)
