6e6a6260276a009f26010add870f1f14a3f1f283
Glen Maddern <glenmaddern@gmail.com>
2010-11-02 13:14:50 +1100

    .

--- app/env.rb
+++ app/env.rb
@@ -1,7 +1,7 @@
 require 'mongoid'
 
 Mongoid.configure do |config|
-  if RAILS_ENV == 'production'
+  if ENV['RAILS_ENV'] == 'production'
     config.master = Mongo::Connection.from_uri(ENV['MONGOHQ_URL']).db("allsofterworlds")
   else
     config.master = Mongo::Connection.new.db("allsofterworlds")
