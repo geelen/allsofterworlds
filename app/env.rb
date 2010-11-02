7015b1b358f04d84d94fe43fbea5f84213c2c003
Glen Maddern <glenmaddern@gmail.com>
2010-11-02 13:13:47 +1100

    trying to get mongo working on heroku

--- app/env.rb
+++ app/env.rb
@@ -1,7 +1,11 @@
 require 'mongoid'
 
 Mongoid.configure do |config|
-  config.master = Mongo::Connection.new.db("allsofterworlds")
+  if RAILS_ENV == 'production'
+    config.master = Mongo::Connection.from_uri(ENV['MONGOHQ_URL']).db("allsofterworlds")
+  else
+    config.master = Mongo::Connection.new.db("allsofterworlds")
+  end
 end
 
 Dir.glob(File.dirname(__FILE__) + '/models/*.rb') { |f| require f }
